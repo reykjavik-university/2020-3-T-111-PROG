5de38e74e2840cfacf31b8b990fb092884fa443d
Hrafn Loftsson <hrafnl@gmail.com>
2020-11-12 10:02:24 +0000

    Updates solution for StringSet

--- exams/practice/string_set.py
+++ exams/practice/string_set.py
@@ -31,27 +31,3 @@ class StringSet:
         return word in self.__words
 
 
-def main():
-    str1 = 'chocolate ice cream and chocolate candy ice bars are my favorite'
-    set1 = StringSet(str1)
-    str2 = 'I like to eat broccoli and fish and ice cream and brussel fish sprouts'
-    set2 = StringSet(str2)
-    print("Set1:", set1)
-    print("Set2:", set2)
-    print("Set1 size:", set1.size())
-    print("Set2 size:", set2.size())
-    the_union = set1 + set2
-    print("Union:", the_union)
-    print("Union size:", the_union.size())
-
-    query = StringSet('chocolate cream fish good rubbish')
-    print("Query:", query)
-    count = 0
-    for i in range(query.size()):
-        if the_union.find(query.at(i)):
-            count += 1
-    
-    print("Query size:", query.size())
-    print("Found in union:", count)
-
-main()
